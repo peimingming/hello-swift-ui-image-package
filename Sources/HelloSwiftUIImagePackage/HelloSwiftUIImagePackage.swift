d0ac592a5634cf962a20eee61633575d1aa44518
peimingming <1019415311@qq.com>
2023-10-16 11:46:53 +0800

    Expose custom image var instead of ImageResource.

--- Sources/HelloSwiftUIImagePackage/HelloSwiftUIImagePackage.swift
+++ Sources/HelloSwiftUIImagePackage/HelloSwiftUIImagePackage.swift
@@ -1,8 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public enum ImageResourceProvider {
-    static let customPencil = ImageResource.cusomtPencil
-    static let customEraser = ImageResource.customEraser
-    static let customBag = ImageResource.customBag
+import SwiftUI
+
+public extension Bundle {
+    static let iconBundle = Bundle.module
+}
+
+public extension Image {
+    static var customPencil: Image { .init(.cusomtPencil) }
+    static var customEraser: Image { .init(.customEraser) }
+    static var customBag: Image { .init(.customBag) }
 }
