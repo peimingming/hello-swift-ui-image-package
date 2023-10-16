95a814a43faa075135bb7ccdf84ec57b9f59bd88
peimingming <1019415311@qq.com>
2023-10-16 11:29:52 +0800

    Implement ImageProvider.

--- Sources/HelloSwiftUIImagePackage/HelloSwiftUIImagePackage.swift
+++ Sources/HelloSwiftUIImagePackage/HelloSwiftUIImagePackage.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public enum ImageResourceProvider {
+    static let customPencil = ImageResource.cusomtPencil
+    static let customEraser = ImageResource.customEraser
+    static let customBag = ImageResource.customBag
+}
